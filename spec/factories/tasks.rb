cb8aca1b1d6f87e34171db8d44902c70423b94e4
Benito Miranda <benomir82@gmail.com>
2018-08-02 17:43:50 -0500

    Generate Task model

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :task do
-
+    subject         { Faker::RickAndMorty.quote #=> "Ohh yea, you gotta get schwifty." }
+    description     { Faker::Lorem.paragraph }
+    priority        { "Medium" }
+    due_date        Time.now + 10.days
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it is valid with a "subject, description, priority, due date, and owner"
-  it is invalid without a "subject"
-  it is invalid without a "description"
-  it is invalid without a "priority"
-  it is invalid without a "due date"
-  it is invalid without a "owner"
+  it "is valid with a subject, description, priority, due date, and owner"
+  it "is invalid without a subject"
+  it "is invalid without a description"
+  it "is invalid without a priority"
+  it "is invalid without a due date"
+  it "is invalid without a owner"
 end
