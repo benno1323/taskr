0b6f5049758ea14e1dfb3de159bb7c5b71e431e8
Benito Miranda <benomir82@gmail.com>
2018-08-02 04:23:10 -0500

    Generate Task model

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,8 +1,5 @@
 FactoryBot.define do
   factory :task do
-    subject "MyString"
-    description "MyText"
-    priority "MyString"
-    due_date "2018-08-02"
+
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it is valid with a "subject, description, priority, due date, and owner"
+  it is invalid without a "subject"
+  it is invalid without a "description"
+  it is invalid without a "priority"
+  it is invalid without a "due date"
+  it is invalid without a "owner"
 end
