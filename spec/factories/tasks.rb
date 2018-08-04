91b7a8e8d60dc0facd2659e7a2facb58971434a8
Benito Miranda <benomir82@gmail.com>
2018-08-03 19:54:26 -0500

    Fix } typo

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :task do
-    subject         { Faker::RickAndMorty.quote #=> "Ohh yea, you gotta get schwifty." }
+    subject         { Faker::RickAndMorty.quote } #=> "Ohh yea, you gotta get schwifty."
     description     { Faker::Lorem.paragraph }
     priority        { "Medium" }
     due_date        Time.now + 10.days
