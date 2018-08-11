4539a2666b1f3a09387461192148f3e7ae6134a9
Benito Miranda <benomir82@gmail.com>
2018-08-10 23:03:09 -0500

    Add index, show, new, create methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,30 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
-    #code
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to @task, notice: 'Task created successfully!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:subject, :description, :priority)
   end
 end
