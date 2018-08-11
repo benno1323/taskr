98dbaebdf674aa9c4978a827bece33c4e018911d
Benito Miranda <benomir82@gmail.com>
2018-08-10 23:05:05 -0500

    Remove due date from model validations as it is setup before validation always

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,20 @@
 class Task < ApplicationRecord
-  validates :subject, :description, :priority, :due_date, presence: true
-
+  validates :subject, :description, :priority, presence: true
+  before_validation :setup_due_date
   belongs_to :user
+
+  def setup_due_date
+    t = DateTime.now
+    count = 0
+
+    while (count <= 20) do
+      if !t.saturday?
+        if !t.sunday?
+          self.due_date = t
+          count = count + 1
+        end
+      end
+      t = t + 1
+    end
+  end
 end
