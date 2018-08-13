91d2f0c57b7de85a136f0854787a665ca71b9ce7
Benito Miranda <benomir82@gmail.com>
2018-08-12 23:53:31 -0500

    Configure Chrome in headless mode

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,42 +2,62 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tasks = Task.all
+    load_tasks
   end
 
   def show
-    @task = Task.find(params[:id])
+    load_task
   end
 
   def new
-    @task = Task.new
+    new_task
   end
 
   def create
-    @task = current_user.tasks.build(task_params)
-    if @task.save
-      redirect_to @task, notice: 'Task created successfully!'
-    else
-      render :new
-    end
+    build_task
+    save_task or render :new
   end
 
   def edit
-    @task = Task.find(params[:id])
+    load_task
   end
 
   def update
-    @task = Task.find(params[:id])
-    if @task.update(task_params)
-      redirect_to @task
-    else
-      render :edit
-    end
+    load_task
+    update_task or render :edit
   end
 
   private
 
   def task_params
     params.require(:task).permit(:subject, :description, :priority)
+  end
+
+  def task_scope
+    Task.all
+  end
+
+  def load_tasks
+    @tasks = task_scope.to_a
+  end
+
+  def new_task
+    @task = Task.new
+  end
+
+  def build_task
+    @task = current_user.tasks.build(task_params)
+  end
+
+  def save_task
+    redirect_to @task, notice: 'Task created successfully!' if @task.save
+  end
+
+  def load_task
+    @task = task_scope.find(params[:id])
+  end
+
+  def update_task
+    redirect_to @task, notice: 'Task updated successfully!' if @task.update(task_params)
   end
 end
